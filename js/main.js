294874f389b29bfcb74051e19c9caa6197fb9dff
KhadijahTheGreat <khadijah.celestine@gmail.com>
2016-07-12 19:08:09 -0400

    More cleanup, added setEntryFeedback function

--- js/main.js
+++ js/main.js
@@ -5,28 +5,28 @@ var itemsLeft = document.getElementById('itemsLeft');
 
 entryForm.addEventListener("submit", enter);
 
-  function isCorrect(input)
-  {
-    books[current].toLowerCase() === input.toLowerCase();
-  }
+function isCorrect(input) {
+  lists.books[current].toLowerCase() === input.toLowerCase();
+}
 
-  function enter(event) {
+function enter(event) {
     var entry = document.getElementById('newEntry');
     var entryFeedback = document.getElementById('entryFeedback');
     var entries = document.getElementById('entries');
-
-    if (books[current] === entry.value)
-    {
+    var current = 0;
+    if (lists.books[current] === entry.value) {
       current++;
-      entryFeedback.innerHTML = 'correct';
-      entryFeedback.style.color = "green";
+      setEntryFeedback('correct', 'green');
       entries.innerHTML += '<tr><td></td><td>' + entry.value + '</td></tr>';
+    } else {
+      setEntryFeedback('wrong', 'red');
     }
-    else
-    {
-      entryFeedback.innerHTML = "wrong";
-      entryFeedback.style.color = "red"
-    }
+
     entry.value = '';
     event.preventDefault();
-  }
+}
+
+function setEntryFeedback(text, color){
+  entryFeedback.innerHTML = text;
+  entryFeedback.style.color = color;
+}
